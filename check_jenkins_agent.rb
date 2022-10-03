b423a8e64185854196da59d534412f7d560fe75b
Robin Ophalvens <robin@ophalvens.eu>
2022-10-03 20:42:51 +0200

    Ruby linting

--- check_jenkins_agent.rb
+++ check_jenkins_agent.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-#
+# frozen_string_literal: true
+
 # This Nagios script checks if a given host is connected to a given Jenkins instance as an
 # eligable Swarm client.
 #
@@ -10,59 +11,63 @@ require 'optparse'
 require 'json'
 require 'httparty'
 
-url=nil
-user=nil
-password=nil
-host=`hostname`
-temp_off_state='WARN'
+url = nil
+user = nil
+password = nil
+host = `hostname`
+temp_off_state = 'WARN'
 
 opt = OptionParser.new
 
-opt.on("--instance [URL]", "-i", "Base URL of the Jenkins Controller") do |f|
+opt.on('--instance [URL]', '-i', 'Base URL of the Jenkins Controller') do |f|
   url = f
 end
 
-opt.on("--host [HOST]", "-h", "Optionally override host to check. This is the hostname by default") do |f|
-  host= f
+opt.on('--host [HOST]', '-h', 'Optionally override host to check. This is the hostname by default') do |f|
+  host = f
 end
 
-opt.on("--user [USER]", "-u", "Authentication username.") do |f|
+opt.on('--user [USER]', '-u', 'Authentication username.') do |f|
   user = f
 end
 
-opt.on("--password [PASSWORD]", "-p", "Authentication password.") do |f|
+opt.on('--password [PASSWORD]', '-p', 'Authentication password.') do |f|
   password = f
 end
 
-opt.on("--temp-offline-state [STATE]", "-t", "Report temporarily offline nodes as: OK, WARN or CRIT. Warning is default") do |f|
+opt.on(
+  '--temp-offline-state [STATE]',
+  '-t',
+  'Report temporarily offline nodes as: OK, WARN or CRIT. Warning is default'
+) do |f|
   temp_off_state = f
 end
 
 opt.parse!
 
-response=nil
+response = nil
 
 begin
   if url.nil?
-    puts "CRITICAL: Please provide a valid Jenkins URL!"
+    puts 'CRITICAL: Please provide a valid Jenkins URL!'
     exit 2
   end
-  auth = {:username => user, :password => password}
-  response = HTTParty.get("#{url}/manage/computer/api/json", :basic_auth => auth)
+  auth = { username: user, password: password }
+  response = HTTParty.get("#{url}/manage/computer/api/json", basic_auth: auth)
   response.parsed_response
-rescue
+rescue StandardError
   puts "UNKNOWN: Unknown error while fetching #{url} computers."
   exit 3
 end
 
 begin
-  data = response['computer'].select {|computer| computer['displayName'] == host}
-rescue
+  data = response['computer'].select { |computer| computer['displayName'] == host }
+rescue StandardError
   puts "UNKNOWN: #{host} is not a part of the #{url} swam cluster!"
   exit 3
 end
 
-if data[0]["offline"]
+if data[0]['offline']
   offline_reason = data[0]['offlineCauseReason']
   if data[0]['temporarilyOffline']
     case temp_off_state
@@ -80,8 +85,8 @@ if data[0]["offline"]
       exit 1
     end
   end
-    puts "CRITICAL: #{host} is offline from #{url}! Reason #{offline_reason}"
-    exit 2
+  puts "CRITICAL: #{host} is offline from #{url}! Reason #{offline_reason}"
+  exit 2
 else
   puts "OK: #{host} is connected to #{url}"
   exit 0
